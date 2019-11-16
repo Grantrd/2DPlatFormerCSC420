cc4396adfd34406af4cc39e41a514bc71b458ab0
Grantrd <31990206+Grantrd@users.noreply.github.com>
2019-11-16 18:11:07 -0500

    found some info, started a project.

---
+++ PlatformingGame/src/platformers/platformingGame/Main.java
@@ -0,0 +1,34 @@
+package platformers.platformingGame;
+
+public class Main implements Runnable {
+	private int width = 1280;
+	private int height = 720; 
+	private Thread thread;
+	
+	public void start() {
+		thread = new Thread(this, "Game");
+		thread.start();
+	}
+	private void init()	{
+		
+	}
+	public void run() {
+		init();
+		while(running)
+		{
+			update();
+			render();
+		}
+	}
+	private void update(){
+		
+	}
+	private void render(){
+		
+	}
+	public static void main(String[] args)
+	{
+		new Main().start();	
+	}
+	
+}
